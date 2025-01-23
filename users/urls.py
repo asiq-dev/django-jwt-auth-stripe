795935b8f2caed4863b2c7de9c163c0aa204cb6e
asiqul islam <asiq.dev@gmail.com>
2025-01-23 23:45:57 +0600

    django-jwt-auth

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .api import UserAPI, logout, logout_all
+
+
+
+
+urlpatterns = [
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path('api/user/', UserAPI.as_view()),
+    path('api/logout/', logout),
+    path('api/logout-all/', logout_all)
+]
